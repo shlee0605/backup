0807e79f049e5d77206dba57753550aebb1013e6
Seunghyun Lee <shlee0605@gmail.com>
2015-01-29 22:55:58 -0500

    modified vim bundle

---
+++ .vim/bundle/install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/Raimondi/delimitMate
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/kchmck/vim-coffee-script
+git clone https://github.com/vim-ruby/vim-ruby
+git clone https://github.com/tpope/vim-sensible
